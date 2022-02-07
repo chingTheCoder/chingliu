7a75eb62c432f1ecbcc263754b44bcec4cd9267c
chingTheCoder <chingliu10@gmail.com>
2022-02-07 19:50:42 +0300

    shifted the site on express server

---
+++ server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+app.set("view engine", "hbs")
+app.use(express.static("public"))
+
+app.get("/", (req, res) => {
+    res.render("index")
+})
+app.listen(3000, () =>
+  console.log('Example app listening on port 3000!'),
+)
